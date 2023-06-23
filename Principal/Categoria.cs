a3dab865d05222abcdf0b949d80e77863d331722
Rafael-PC <rafaelreinertd@hotmail.com>
2023-06-23 16:52:38 -0300

    Padroes Tipo Ataque

--- Principal/Categoria.cs
+++ Principal/Categoria.cs
@@ -8,8 +8,25 @@ namespace projeto1_RPG.Principal
 {
     internal class Categoria
     {
-        public string Elemental { get; set; }
-        public string Fisica { get; set; }
+        public static class Elemento
+        {
+            public static readonly string Neutro = "Neutro";
+            public static readonly string Fogo = "Fogo";
+            public static readonly string Gelo = "Gelo";
+            public static readonly string Eletrico = "Eletrico";
+            public static readonly string Divino = "Divino";
+            public static readonly string Trevas = "Trevas";
+            public static readonly string Veneno = "Veneno";
+        }
+        public static class Fisico
+        {
+            public static readonly string Impacto = "Impacto";
+            public static readonly string Cortante = "Cortante";
+            public static readonly string Perfurante = "Perfurante";
+        }
+
+        public string Elemental { get; private set; }
+        public string Fisica { get; private set; }
 
         public Categoria(string elemental = null, string fisica = null)
         {
