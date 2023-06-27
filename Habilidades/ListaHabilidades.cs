2ada9084f6cd8716f26481b09fc745f6d2b27f0a
Rafael-PC <rafaelreinertd@hotmail.com>
2023-06-27 17:54:33 -0300

    nova habilidade guerreiro

--- Habilidades/ListaHabilidades.cs
+++ Habilidades/ListaHabilidades.cs
@@ -14,11 +14,13 @@ namespace projeto1_RPG.Habilidades
 
         public ListaHabilidades()
         {
-            HabilidadesOfensivas.Add(new HabilidadeOfensiva(1, "Bola de Fogo", "Bola de Fogo", 10, new[] { "Mago" }, new Principal.Ataque(new Principal.Categoria(Categoria.Elemento.Fogo, Categoria.Fisico.Magia), 10, 14), false, false));
-            HabilidadesOfensivas.Add(new HabilidadeOfensiva(2, "Raio Eletrico", "Raio Eletrico", 10, new[] { "Mago" }, new Principal.Ataque(new Principal.Categoria(Categoria.Elemento.Eletrico, Categoria.Fisico.Magia), 10, 14), false, false));
-            HabilidadesOfensivas.Add(new HabilidadeOfensiva(3, "Halito Gelado", "Halito Gelado", 10, new[] { "Mago" }, new Principal.Ataque(new Principal.Categoria(Categoria.Elemento.Gelo, Categoria.Fisico.Magia), 10, 14), false, false));
-            HabilidadesOfensivas.Add(new HabilidadeOfensiva(4, "Golpe Furtivo", "Golpe Furtivo", 5, new[] { "Ladrão" }, new Principal.Ataque(new Principal.Categoria(Categoria.Elemento.Neutro, Categoria.Fisico.Perfurante), 10, 20), false, true));
-            HabilidadesOfensivas.Add(new HabilidadeOfensiva(5, "Inflingir Ferimento", "Inflingir Ferimento", 7, new[] { "Clérigo" }, new Principal.Ataque(new Principal.Categoria(Categoria.Elemento.Divino, Categoria.Fisico.Magia), 8, 12), false, false));
+            HabilidadesOfensivas.Add(new HabilidadeOfensiva(1, "Bola de Fogo", "Bola de Fogo", 10, new[] { "Mago" }, new Ataque(new Categoria(Categoria.Elemento.Fogo, Categoria.Fisico.Magia), 10, 14), false, false));
+            HabilidadesOfensivas.Add(new HabilidadeOfensiva(2, "Raio Eletrico", "Raio Eletrico", 10, new[] { "Mago" }, new Ataque(new Categoria(Categoria.Elemento.Eletrico, Categoria.Fisico.Magia), 10, 14), false, false));
+            HabilidadesOfensivas.Add(new HabilidadeOfensiva(3, "Halito Gelado", "Halito Gelado", 10, new[] { "Mago" }, new Ataque(new Categoria(Categoria.Elemento.Gelo, Categoria.Fisico.Magia), 10, 14), false, false));
+            HabilidadesOfensivas.Add(new HabilidadeOfensiva(4, "Golpe Furtivo", "Golpe Furtivo", 5, new[] { "Ladrão" }, new Ataque(new Categoria(Categoria.Elemento.Neutro, Categoria.Fisico.Perfurante), 10, 20), false, true));
+            HabilidadesOfensivas.Add(new HabilidadeOfensiva(5, "Inflingir Ferimento", "Inflingir Ferimento", 7, new[] { "Clérigo" }, new Ataque(new Categoria(Categoria.Elemento.Divino, Categoria.Fisico.Magia), 8, 12), false, false));
+            HabilidadesOfensivas.Add(new HabilidadeOfensiva(6, "Ataque Impulsionado", "Ataque Impulsionado", 9, new[] { "Guerreiro" }, new Ataque(new Categoria(Categoria.Elemento.Neutro, Categoria.Fisico.Impacto), 12, 18), false, false));
+
         }
     }
 }
